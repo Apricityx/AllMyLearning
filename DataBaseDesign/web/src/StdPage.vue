60ba03c7050ff57ba9c7b5c9693a0cc39715497f
Apricityx <Apricityx@qq.com>
2024-06-11 17:56:43 +0800

    Feat: Read Data From StdPage

--- DataBaseDesign/web/src/StdPage.vue
+++ DataBaseDesign/web/src/StdPage.vue
@@ -1,43 +1,45 @@
 <script setup lang="ts">
 import axios from 'axios';
-import {debug} from '@/utils/debug';
+import { debug } from '@/utils/debug';
 import InfoContainer from "@/component/StdPage/InfoContainer.vue";
-import {onMounted, ref, provide} from "vue";
+import { onMounted, ref } from "vue";
 
 const temp_data = ref<string>('');
 const get_info = () => {
     const url = "http://localhost:3000/api/v1/auth/get_info";
-    const cookies: string = document.cookie
+    const cookies: string = document.cookie;
     debug("获取到的cookies:" + cookies);
-// 从Cookie中获取accessToken
-    const accessToken: string = cookies.split(';').find((item) => item.includes('accessToken')).split('accessToken=')[1];
+
+    // 从Cookie中获取accessToken
+    const accessToken: string = cookies.split(';').find((item) => item.includes('accessToken'))?.split('accessToken=')[1];
     debug("获取到的accessToken:" + accessToken);
-// 发送请求
+
+    // 发送请求
     axios.post(url, {
         accessToken: accessToken
     })
         .then(response => {
-            debug('ALL THE INFO: ' + JSON.stringify(response.data));
+            // debug('ALL THE INFO: ' + JSON.stringify(response.data));
             temp_data.value = JSON.stringify(response.data);
         })
         .catch(error => {
             alert('请求失败，请重新登录再试');
-            document.cookie = 'accessToken= expires=Thu, 01 Jan 1970 00:00:00 GMT';
+            document.cookie = 'accessToken=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
             window.location.href = './login';
             debug('Error:' + error);
         });
 }
+
 onMounted(() => {
     get_info();
-})
+});
 </script>
 
 <template>
     <div style="border:2px solid black">
-        <InfoContainer :props="temp_data"/>
+        <InfoContainer :httpResult="temp_data"/>
     </div>
 </template>
 
 <style scoped>
-
 </style>
--- DataBaseDesign/web/src/component/StdPage/InfoContainer.vue
+++ DataBaseDesign/web/src/component/StdPage/InfoContainer.vue
@@ -1,26 +1,44 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import {debug} from "@/utils/debug";
-import {watch} from 'vue';
+import {ref, watch} from 'vue';
+import {defineProps} from 'vue';
 
-const activeName = ref<string>('1')
 const props = defineProps<{
-    props: string
-}>()
-watch(() => props, (newVal) => {
-        console.log('请求结果:', newVal);
-        // 在这里执行相应的代码
-});
+    httpResult: string
+}>();
+// 接受一个 httpResult 参数，用于接受父组件传递的数据
+
+const activeName = ref<string>('1');
+const parsedResult = ref<any>({});
+// 监听 httpResult 的变化，执行相应的代码
+watch(
+    () => props.httpResult,
+    (newVal) => {
+        if (newVal) {
+            parsedResult.value = JSON.parse(newVal);
+            console.log('解析后的请求结果:', parsedResult.value);
+            console.log('请求结果:', newVal);
+            // 在这里执行相应的代码
+        }
+    },
+    {immediate: true}
+);
 </script>
 
 <template>
     <el-collapse v-model="activeName" accordion>
-        <el-collapse-item title="Consistency" name="1">
-            {{ props }}
+        <el-collapse-item
+            v-for="tutor in parsedResult"
+            :key="tutor.TutorID"
+            :title="tutor.TutorName"
+            :name="tutor.TutorID.toString()"
+        >
+            <div>
+                <p>Tutor Info: {{ tutor.TutorInfo }}</p>
+                <p>Status: {{ tutor.Status }}</p>
+            </div>
         </el-collapse-item>
     </el-collapse>
 </template>
 
 <style scoped>
-
 </style>
