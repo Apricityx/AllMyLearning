f6e94fc7bf36e371f82c30b0e5ae2bdbd3c1d138
apricityx <apricityx@qq.com>
2024-07-04 09:35:54 +0800

    完成Pic页面

--- UniappLearning/web/src/pages/PicView.vue
+++ UniappLearning/web/src/pages/PicView.vue
@@ -1,26 +1,42 @@
 <script setup lang="ts">
 import axios from 'axios'
 import {ref} from 'vue'
+
+const url = ref('https://meme.3sqrt7.com/api/image')
+const nextPic = () => {
+    url.value = ''
+    const random = Math.floor(Math.random() * 1000)
+    url.value = 'https://meme.3sqrt7.com/api/image?index=' + random
+}
 </script>
 
 <template style="overflow: hidden">
     <view>
         <div class="ImgContainer">
-            <v-card width="100%">
-                <v-img
-                    height="fit-content"
-                    lazy-src="../../static/img/Loading.jpg"
-                    max-width="500"
-                    src="https://meme.3sqrt7.com/api/image"
-                >
-                </v-img>
-                <!--            <v-btn>下一张</v-btn>-->
-            </v-card>
+            <v-img
+                style="box-shadow: black 0 0 10px 1px;border-radius: 5px"
+                height="fit-content"
+                lazy-src="../../static/img/Loading2.jpg"
+                max-height="90%"
+                max-width="90%"
+                :src="url"
+            >
+                <template v-slot:placeholder>
+                    <div class="d-flex align-center justify-center fill-height">
+                        <v-progress-circular
+                            color="grey-lighten-4"
+                            indeterminate
+                        ></v-progress-circular>
+                    </div>
+                </template>
+            </v-img>
+
+            <!--            <v-btn>下一张</v-btn>-->
         </div>
     </view>
-    <div class="NextButton">
+    <div class="NextButton" @click="nextPic()">
         <v-fab icon="$vuetify" width="50" height="50">
-            <v-icon icon="mdi-access-point-off"></v-icon>
+            <v-icon icon="mdi-skip-next-outline"></v-icon>
             <v-progress-circular
                 color="grey-lighten-4"
                 indeterminate
@@ -37,7 +53,7 @@ import {ref} from 'vue'
     height: 50px;
     position: fixed;
     bottom: 10%;
-    right: 10%;
+    right: 5%;
     //left: 50%;
     //transform: translateX(-50%);
 }
